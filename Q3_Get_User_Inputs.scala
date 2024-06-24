80dd8012147143c783b9d0d2a230075d4b9d6120
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-24 23:02:08 +0530

    Create Q3_Get_User_Inputs.scala

---
+++ Q3_Get_User_Inputs.scala
@@ -0,0 +1,15 @@
+def salCalc(normalHrs : Int, otHours : Int) : Unit = {
+    val normalPay = 250
+    val OT_pay = 85
+    val normalWorkHours = 40
+    val OT_Hours = 30
+    val taxRate = 0.12
+
+    var ot_salary = otHours * OT_pay
+    var normal_salary = normalPay * normalWorkHours
+    var grossSalary = ot_salary + normal_salary
+    var tax = grossSalary * taxRate
+    var net_salary = grossSalary - tax
+    println("Salary : " + net_salary)
+}
+
